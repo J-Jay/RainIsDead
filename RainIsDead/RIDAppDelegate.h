8f5325f05a2fed33fc5876a9a5d899de53522449
J-Jay <jjmerillon@gmail.com>
2013-11-11 12:28:16 +0100

    Initial Commit

---
+++ RainIsDead/RIDAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  RIDAppDelegate.h
+//  RainIsDead
+//
+//  Created by Jean-Jacques Mérillon on 11/11/2013.
+//  Copyright (c) 2013 Jean-Jacques Mérillon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RIDAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ RainIsDead/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  RainIsDead
+//
+//  Created by Jean-Jacques Mérillon on 11/11/2013.
+//  Copyright (c) 2013 Jean-Jacques Mérillon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "RIDAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([RIDAppDelegate class]));
+    }
+}
